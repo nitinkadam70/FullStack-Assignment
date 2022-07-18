1f355adaccc1cfef8dc98fb0d973c092d5cea10a
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-07-18 22:27:30 +0530

    add

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const connection = require("./database")
 const cors = require("cors");
 require("dotenv").config();
 
+
+//routes
+const articleRouter = require("./Routes/articles.routes")
+const authRouter = require("./Routes/auth.routes")
 
 const app = express();
 
@@ -10,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use("/auth", authRouter);
+app.use("/user", articleRouter);
+
 //Homepage route
 app.get("/", (req, res) => {
     return res.send("Homepage");
@@ -17,6 +25,13 @@ app.get("/", (req, res) => {
 
 const Port = process.env.PORT || 8080;
 
-app.listen(Port, () => {
-    console.log(`server running on ${Port}`)
+app.listen(Port, async () => {
+    try {
+        await connection
+        console.log("Connected to DB successfully");
+    }
+    catch {
+        console.log("Failed to connect DB")
+    }
+    console.log(`server running on localhost:${Port}`)
 })
